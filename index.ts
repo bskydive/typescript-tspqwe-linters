3500045b0f68398589565f7b9cd9a4bcc82d7804
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2021-03-11 23:59:31 +0300

    stackblitz fix

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 // stub to make stackblitz works
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+// appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
