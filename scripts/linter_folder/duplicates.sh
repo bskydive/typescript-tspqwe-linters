1b0c224e1744d1ee0e4cedf94689fe0b8f8245fe
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2022-08-15 15:15:08 +0300

    scripts

--- scripts/linter_folder/duplicates.sh
+++ scripts/linter_folder/duplicates.sh
@@ -3,22 +3,22 @@
 # you need at least one commit in local git repo
 # copy .jscpd* config files if you want to run in project folder
 # npm i jscpd jscpd-badge-reporter
-echo -e "\n# COPY PASTE" > log/duplicates.md
+echo -e "\n# COPY PASTE" > ../../log/duplicates.md
 
-echo -e "\n# All" >> log/duplicates.md
+echo -e "\n# All" >> ../../log/duplicates.md
 npm run lint:cp
-cat log/jscpd/jscpd-report.md >> log/duplicates.md
+cat log/jscpd/jscpd-report.md >> ../../log/duplicates.md
 
-echo -e "\n# Html" >> log/duplicates.md
+echo -e "\n# Html" >> ../../log/duplicates.md
 npm run lint:cp-html
-cat log/jscpd-html/jscpd-report.md >> log/duplicates.md
+cat log/jscpd-html/jscpd-report.md >> ../../log/duplicates.md
 
-echo -e "\n# CSS" >> log/duplicates.md
+echo -e "\n# CSS" >> ../../log/duplicates.md
 npm run lint:cp-css
-cat log/jscpd-css/jscpd-report.md >> log/duplicates.md
+cat log/jscpd-css/jscpd-report.md >> ../../log/duplicates.md
 
-echo -e "\n# TS" >> log/duplicates.md
+echo -e "\n# TS" >> ../../log/duplicates.md
 npm run lint:cp-ts
-cat log/jscpd-ts/jscpd-report.md >> log/duplicates.md
+cat log/jscpd-ts/jscpd-report.md >> ../../log/duplicates.md
 
-cat log/duplicates.md >> log/log.md
+cat log/duplicates.md >> ../../log/log.md
--- scripts/linter_folder/tslint.sh
+++ scripts/linter_folder/tslint.sh
@@ -1,16 +1,16 @@
 ############################################# TS LINT
 
-echo -e "\n# TSLINT" > log/tslint.md
+echo -e "\n# TSLINT" > ../../log/tslint.md
 
-npm run lint:ts > log/tslint.log
+npm run lint:ts > ../../log/tslint.log
 # grep WARNING: log/tslint.log | colrm 1 16 | sort | uniq | less
-# grep WARNING: log/tslint.log | colrm 1 16 | sort | uniq > log/tslint.uniq.log
-cat log/tslint.log | awk -F'(' '{print $1}' | sort | uniq > log/tslint.files.log
-wc -l log/tslint.files.log >> log/tslint.md
-cat log/tslint.log | awk -F':' '{print $3}' | sort | uniq > log/tslint.issues.log
-wc -l log/tslint.issues.log >> log/tslint.md
-cat log/tslint.log | awk -F':' '{print $2}' | sort | uniq > log/tslint.rules.log 
-wc -l log/tslint.rules.log >> log/tslint.md
+# grep WARNING: log/tslint.log | colrm 1 16 | sort | uniq > ../../log/tslint.uniq.log
+cat log/tslint.log | awk -F'(' '{print $1}' | sort | uniq > ../../log/tslint.files.log
+wc -l log/tslint.files.log >> ../../log/tslint.md
+cat log/tslint.log | awk -F':' '{print $3}' | sort | uniq > ../../log/tslint.issues.log
+wc -l log/tslint.issues.log >> ../../log/tslint.md
+cat log/tslint.log | awk -F':' '{print $2}' | sort | uniq > ../../log/tslint.rules.log 
+wc -l log/tslint.rules.log >> ../../log/tslint.md
 
-cat log/tslint.md >> log/log.md
+cat log/tslint.md >> ../../log/log.md
 
